4c1c0057351b820bb3816cad1049791f11071819
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2024-02-06 13:00:43 -0300

    feat: add application assets and fonts and add login page

---
+++ lib/src/pages/login/login_router.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+
+import '../../data/repositories/user/i_user_repository.dart';
+import '../../data/repositories/user/user_repository.dart';
+import '../../data/services/user/i_user_login_service.dart';
+import '../../data/services/user/user_login_service.dart';
+import 'login_controller.dart';
+import 'login_page.dart';
+
+class LoginRouter extends FlutterGetItPageRouter {
+  const LoginRouter({super.key});
+
+  @override
+  List<Bind<Object>> get bindings => [
+        Bind.lazySingleton<IUserRepository>((i) => UserRepository(restClient: i())),
+        Bind.lazySingleton<IUserLoginService>((i) => UserLoginService(userRepository: i())),
+        Bind.lazySingleton((i) => LoginController(userLoginService: i())),
+      ];
+
+  @override
+  String get routeName => '/login';
+
+  @override
+  WidgetBuilder get view => (_) => const LoginPage();
+}
